c5f7b6d818cf97ed04a700c79a47bf69b289132c
Arnav Vashisth <arnavvst2020@gmail.com>
2021-04-30 21:23:19 +0530

    Update newitter.js

--- newitter.js
+++ newitter.js
@@ -2,6 +2,11 @@
 function addUser() {
     var checkBox = document.querySelector(".check");
     if (checkBox.checked == true) {
+      staff_values= document.getElementById("staff").value;
+      if (staff_values == 2942021){
+        staff_values = true; 
+        localStorage.setItem("staff", staff_values);
+      }
       user_name = document.getElementById("user_name").value;
       password = document.getElementById("password").value;
         localStorage.setItem("user_name", user_name);
@@ -15,4 +20,5 @@ function addUser() {
   
      
     }
+    
     
